a802f6a699456f3f27f8393a463d3e3ed3f48abf
Nguyen Thanh Phuc <thanhphucnguyen54@gmail.com>
2025-04-22 23:42:41 +0700

    feat: Add customer review, footer and product

--- src/app.js
+++ src/app.js
@@ -14,3 +14,57 @@ navLink.forEach(link => {
         hamburger.classList.toggle("ri-close-large-line");
     });
 });
+
+/* --------------------SHOW SCROLL UP--------------------*/
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+    if(this.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+    }else {
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-4");
+    }
+}
+window.addEventListener("scroll", scrollUp);
+
+
+/* --------------------CHANGE BACKGROUND HEADER--------------------------*/
+const scrollHeader = () => {
+    const scrollUpBtn = document.getElementById("navbar");
+    if(this.scrollY >= 250) {
+        scrollUpBtn.classList.add("border-b", "border-yellow-500");
+    }else {
+        scrollUpBtn.classList.remove("border-b", "border-yellow-500");
+    }
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* --------------------SWIPER---------------------------*/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    grapCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  
+  });
